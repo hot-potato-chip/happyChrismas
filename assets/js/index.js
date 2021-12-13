5df377360d63aab13b218209967cb89edbe0e724
blackpig <15184486929@163.com>
2021-12-13 17:35:52 +0800

    统一单位,响应适配

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,7 @@
 const START_SCROLL_DISTANCE = 0
-const END_SCROLL_DISTANCE = 4520
-const SCROLL_DISTANCE = END_SCROLL_DISTANCE - START_SCROLL_DISTANCE
+let END_SCROLL_DISTANCE = 5 * window.innerHeight
+let SCROLL_DISTANCE = END_SCROLL_DISTANCE - START_SCROLL_DISTANCE
 const ROTATE_DEG = 120
-const OPEN_DEG = 100
 
 const ENVELOPE_START_TRANS_RATIO = 0.5
 const ENVELOPE_TRANS_DISTANCE = 10
@@ -14,32 +13,41 @@ let open = false
 const envelope = document.querySelector('#envelope')
 const page = document.querySelector('#page')
 
-envelope.addEventListener("click",e=>{
-  open=true
-  envelope.style.setProperty('cursor','initial')
+envelope.addEventListener("click", e => {
+  open = true
+  envelope.style.setProperty('cursor', 'initial')
 })
 
-document.addEventListener('scroll', e => {
-  if (!open){
-    e.preventDefault
-    scrollTo(0,0)
+const zeroToOne = (num) => {
+  if (num < 0) return 0
+  if (num > 1) return 1
+  return num
+}
+
+const adjustEnvelope = () => {
+  if (!open) {
+    scrollTo(0, 0)
     return false
   }
-  const envelopeTop = document.querySelector('#envelopeTop')
-  let scrollRatio = ((envelope.offsetTop - START_SCROLL_DISTANCE) / SCROLL_DISTANCE)
+  let scrollRatio = Math.min((envelope.offsetTop - START_SCROLL_DISTANCE) / SCROLL_DISTANCE, 1)
   let rotatedeg = Math.ceil(scrollRatio * ROTATE_DEG)
-  envelope.style.setProperty('--rotatedeg',rotatedeg + 'deg')
-  if (rotatedeg >= OPEN_DEG) {
-
-  }
+  envelope.style.setProperty('--rotatedeg', rotatedeg + 'deg')
   if (scrollRatio >= ENVELOPE_START_TRANS_RATIO) {
-    envelope.style.setProperty('--translateYDistance', Math.ceil(((scrollRatio - ENVELOPE_START_TRANS_RATIO) / (1 - ENVELOPE_START_TRANS_RATIO)) * ENVELOPE_TRANS_DISTANCE) + 'rem')
+    envelope.style.setProperty('--translateYDistance',
+    zeroToOne((scrollRatio - ENVELOPE_START_TRANS_RATIO) / (1 - ENVELOPE_START_TRANS_RATIO)) * ENVELOPE_TRANS_DISTANCE + 'rem')
   }
   if (scrollRatio >= PAGE_START_TRANS_RATIO) {
-    envelope.style.setProperty('--translateZDistance','100rem')
-    page.style.setProperty('--translateYDistance',-Math.ceil((scrollRatio-PAGE_START_TRANS_RATIO)/(1-PAGE_START_TRANS_RATIO) * PAGE_TRANS_DISTANCE)+'rem')
+    envelope.style.setProperty('--translateZDistance', '100rem')
+    page.style.setProperty('--translateYDistance',
+    -zeroToOne((scrollRatio - PAGE_START_TRANS_RATIO) / (1 - PAGE_START_TRANS_RATIO)) * PAGE_TRANS_DISTANCE + 'rem')
   } else {
-    envelope.style.setProperty('--translateZDistance','0rem')
+    envelope.style.setProperty('--translateZDistance', '0rem')
   }
-})
+}
 
+window.addEventListener('resize', () => {
+  END_SCROLL_DISTANCE = 5 * window.innerHeight
+  SCROLL_DISTANCE = END_SCROLL_DISTANCE - START_SCROLL_DISTANCE
+  adjustEnvelope()
+})
+document.addEventListener('scroll', adjustEnvelope)
