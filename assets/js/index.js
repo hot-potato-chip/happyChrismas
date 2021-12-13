f3687d6423765a22328e48d3dfe514cb9a8235fd
blackpig <15184486929@163.com>
2021-12-13 16:27:35 +0800

    完成信封雏形

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,18 @@
-const START_SCROLL_DISTANCE = 100
-const END_SCROLL_DISTANCE = 4500
+const START_SCROLL_DISTANCE = 0
+const END_SCROLL_DISTANCE = 4520
 const SCROLL_DISTANCE = END_SCROLL_DISTANCE - START_SCROLL_DISTANCE
 const ROTATE_DEG = 120
-const START_TRANS_RATIO = 0.5
-const TRANS_DISTANCE = 10
+const OPEN_DEG = 100
+
+const ENVELOPE_START_TRANS_RATIO = 0.5
+const ENVELOPE_TRANS_DISTANCE = 10
+
+const PAGE_START_TRANS_RATIO = 0.8
+const PAGE_TRANS_DISTANCE = 15
 
 let open = false
-const envelope = document.querySelector('.envelope')
+const envelope = document.querySelector('#envelope')
+const page = document.querySelector('#page')
 
 envelope.addEventListener("click",e=>{
   open=true
@@ -19,11 +25,21 @@ document.addEventListener('scroll', e => {
     scrollTo(0,0)
     return false
   }
-  const envelopeTop = document.querySelector('.envelopeTop')
+  const envelopeTop = document.querySelector('#envelopeTop')
   let scrollRatio = ((envelope.offsetTop - START_SCROLL_DISTANCE) / SCROLL_DISTANCE)
-  envelopeTop.style.setProperty('--rotatedeg', Math.ceil(scrollRatio * ROTATE_DEG) + 'deg')
-  if (scrollRatio >= START_TRANS_RATIO) {
-    envelope.style.setProperty('--translateYDistance', Math.ceil(((scrollRatio - START_TRANS_RATIO) / START_TRANS_RATIO) * TRANS_DISTANCE) + 'rem')
+  let rotatedeg = Math.ceil(scrollRatio * ROTATE_DEG)
+  envelope.style.setProperty('--rotatedeg',rotatedeg + 'deg')
+  if (rotatedeg >= OPEN_DEG) {
+
+  }
+  if (scrollRatio >= ENVELOPE_START_TRANS_RATIO) {
+    envelope.style.setProperty('--translateYDistance', Math.ceil(((scrollRatio - ENVELOPE_START_TRANS_RATIO) / (1 - ENVELOPE_START_TRANS_RATIO)) * ENVELOPE_TRANS_DISTANCE) + 'rem')
+  }
+  if (scrollRatio >= PAGE_START_TRANS_RATIO) {
+    envelope.style.setProperty('--translateZDistance','100rem')
+    page.style.setProperty('--translateYDistance',-Math.ceil((scrollRatio-PAGE_START_TRANS_RATIO)/(1-PAGE_START_TRANS_RATIO) * PAGE_TRANS_DISTANCE)+'rem')
+  } else {
+    envelope.style.setProperty('--translateZDistance','0rem')
   }
 })
 
